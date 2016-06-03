82e37bd80d69394d2852c068d095712dd0250af2
BOURGUET Antoine <antoine.bourguet@april.interne.fr>
2016-06-03 15:41:59 +0200

    first commit

---
+++ UnitTestProject2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("UnitTestProject2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("April Technologies")> 
+<Assembly: AssemblyProduct("UnitTestProject2")> 
+<Assembly: AssemblyCopyright("Copyright © April Technologies 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("ff9930b2-6013-4117-a038-6b8c7233088f")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ UnitTestProject2/UnitTest1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestMethod1()
+        Assert.IsFalse(False)
+    End Sub
+
+    <TestMethod()>
+    Public Sub ShouldFail()
+        Assert.IsTrue(False)
+    End Sub
+
+End Class
